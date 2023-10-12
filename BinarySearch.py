6b7e27f846257ec0f391886e050cba6c86dd2ff2
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2023-10-12 16:29:30 -0500

    added 33. Binary search in rotated sorted array

--- BinarySearch.py
+++ BinarySearch.py
@@ -16,7 +16,8 @@ def minEatingSpeed(piles: list[int], h: int) -> int:
         
         return m
 
-'''153. Minimum of rotated sorted array. If left is bigger than right, than binary search the right half, else binary search the left half. When left is less than righ  Time complexity O(logn), space complexity O(1)'''
+'''153. Minimum of rotated sorted array. If left is bigger than right, than binary search the right half, else binary search the left half. 
+When left is less than righ  Time complexity O(logn), space complexity O(1)'''
 def findMin(nums: list[int]) -> int:     
     l,r = 0, len(nums)-1
     m = (l + r) // 2
@@ -30,5 +31,26 @@ def findMin(nums: list[int]) -> int:
             r = m
             m = (l + r) // 2
 
-print(findMin([10,11,12,13,14,15,16,17,1,2,3,4,5,6,7,8]))
-                
+
+'''33. binary Search in rotated sorted array. In each loop, check if middle is in left half (or if array is not rotated) or right half, and check accordingly'''
+def search(nums: list[int], target: int) -> int:
+    l,r = 0, len(nums)-1
+
+    while l <= r:
+        m = (l + r) // 2
+        if target == nums[m]:
+            return m
+        if nums[m] >= nums[l]:
+            if nums[m] < target or nums[l] > target:
+                l = m + 1
+            else: 
+                r = m - 1
+        else:
+            if nums[m] > target or nums[r] < target:
+                r = m - 1
+            else:
+                l = m + 1
+
+
+    return -1
+    
