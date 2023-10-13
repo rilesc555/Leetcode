8b8127a995a0bd409495956fadd9dbee31f1d536
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2023-10-13 15:54:55 -0500

    added 21. Merge two sorted lists

--- LinkedList.py
+++ LinkedList.py
@@ -17,3 +17,25 @@ def reverseList(self, head: ListNode) -> ListNode:
     
     return prev
 
+# 21 merge two sorted lists. Starting to see how this works
+def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+    dummy = ListNode()
+    tail = dummy
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next
+    
+    if list1:
+        tail.next = list1
+    if list2:
+        tail.next = list2
+
+    return dummy.next
+
+
