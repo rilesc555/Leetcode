7eba274311943a0c8133a25be83b03d4b4786cf5
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2023-10-13 10:50:38 -0500

    Added 981. Time-based key-value store

--- BinarySearch.py
+++ BinarySearch.py
@@ -53,4 +53,43 @@ def search(nums: list[int], target: int) -> int:
 
 
     return -1
+
+'''981. Time based key-value store. Dictionary of with values as keys and list of tuples (timestamp, value) as values.'''
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        values = self.store.get(key)
+        if not values:
+            return res
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][0] == timestamp:
+                res = values[m][1]
+                break
+            elif values[m][0] > timestamp:
+                r = m - 1
+            else:
+                res = values[m][1]
+                l = m + 1
+        return res
+
+
+values = TimeMap()
+
+values.set("foo", "bar", 1)
+print(values.get("foo", 1))
+print(values.get("foo", 3))
+values.set("foo", "bar2", 4)
+print(values.get("foo", 4))
     
