2d4d5eadbf9987e69f370dc6925a2e63bfa25030
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2023-10-20 10:17:16 -0500

    543 Diameter of binary tree

--- Trees.py
+++ Trees.py
@@ -25,3 +25,22 @@ class MaxDepth:
             return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+
+#543 Diameter of binary tree. Basic DFS, comparing taking node with greatest diameter starting at bottom
+class Diameter:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        res = [0]
+
+        def dfs(root):
+            if not root:
+                return -1
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            res[0] = max(res[0], 2 + left + right)
+
+            return 1 + max(left, right)
+
+        dfs(root)
+        return res[0]
